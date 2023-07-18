4b08c13f6526e66385d1d3fb8071d0e27c32c7fd
Andrew <drewstandes@gmail.com>
2023-07-18 19:43:23 +0100

    Docker

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uw-&1u+j71g69d94_#ye7-f8)@&8loepv+6nsg=ad=cp66xv%1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -121,4 +121,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
